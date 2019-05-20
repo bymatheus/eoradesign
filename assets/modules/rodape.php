642ca9781d88375e803cc7d591b40308d297daa1
Matheus Camargo <matheuscx@outlook.com>
2019-05-20 20:02:44 -0300

    Correção de identação

--- assets/modules/rodape.php
+++ assets/modules/rodape.php
@@ -1,7 +1,6 @@
         <footer>
     
         </footer>
-        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="<?=$h?>assets/js/bootstrap.min.js"></script>
     </body>
